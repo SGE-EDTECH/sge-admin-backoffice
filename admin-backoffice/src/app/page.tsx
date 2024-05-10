d5330cfdb518778980e25741f334f164a598b798
DG-Tech00 <douglasalvessouza0@gmail.com>
2024-05-09 21:02:28 -0300

    SETUP INICIAL FRONT

---
+++ admin-backoffice/src/app/page.tsx
@@ -0,0 +1,13 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div>
+        <h1 className={styles.h1}>
+          Hello World
+        </h1>
+      </div>
+    </main>
+  );
+}
